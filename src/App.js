9fd679bdd53e05bc5cb71a83cbc732f03202c957
Shubham Kashyap <hex.media.1800@gmail.com>
2021-10-15 10:57:13 +0530

    sass with custom font

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 className='clr-white'> Electron / React Desktop Allication!! </h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
